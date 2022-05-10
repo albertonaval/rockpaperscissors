d5ab47cc93a88ca342f90cd71be34483a96d1c9b
Alberto Naval <albertonaval.dev@gmail.com>
2022-05-10 17:46:53 +0100

    Start UI

--- script.js
+++ script.js
@@ -37,11 +37,19 @@ function playRound(playerSelection, computerSelection) {
   }
   alert(playRound(playerSelection, computerSelection));
 
-for (let i = 0; i < 0; i++) {
-let computerScore = 0;
-let playerScore = 0;
-}
-  function gameIsOver() {
-    playerScore === '5' || computerScore === '5';
-  }
+// for (let i = 0; i < 0; i++) {
+// let computerScore = 0;
+// let playerScore = 0;
+// }
+//   function gameIsOver() {
+//     playerScore === '5' || computerScore === '5';
+//   }
+
+// UI
+// In our UI, the player should be able to play the game by clicking on buttons rather than typing their answer in a prompt.
+
+//     For now, remove the logic that plays exactly five rounds.
+//     Create three buttons, one for each selection. Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+//     Add a div for displaying results and change all of your console.logs into DOM methods.
+//     Display the running score, and announce a winner of the game once one player reaches 5 points.
 
